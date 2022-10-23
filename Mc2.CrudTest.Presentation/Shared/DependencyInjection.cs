ca07f0267b64039e7d736ae3eeb01340a5778803
Parsa <Mehdipourparsa@gmail.com>
2022-10-23 22:54:03 +0400

    feat : Add DependencyInjection class

---
+++ Mc2.CrudTest.Presentation/Shared/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Mc2.CrudTest.Shared.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Mc2.CrudTest.Shared
+{
+    public static class DependencyInjection
+    {
+        public static void SetupSharedApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(assembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+
+            services.AddValidatorsFromAssembly(assembly);
+        }
+    }
+}
