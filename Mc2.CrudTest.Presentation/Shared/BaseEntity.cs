092826145f9ccbeecfe3ecd04f0a9d33654404fb
Parsa <Mehdipourparsa@gmail.com>
2022-10-23 22:52:10 +0400

    feat : Add BaseEntity model

---
+++ Mc2.CrudTest.Presentation/Shared/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mc2.CrudTest.Shared
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        protected BaseEntity()
+        {
+            CreatedDate = DateTime.Now; ;
+        }
+    }
+
+    public abstract class BaseEntity : BaseEntity<long>
+    {
+
+    }
+}
