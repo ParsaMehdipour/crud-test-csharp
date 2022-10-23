7ace55d4bccc62717e30d3efc60c563ca36f7802
Parsa <Mehdipourparsa@gmail.com>
2022-10-23 22:53:48 +0400

    feat : Add RegularExpressionConsts class

---
+++ Mc2.CrudTest.Presentation/Shared/Consts/RegularExpressionConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace Mc2.CrudTest.Shared.Consts
+{
+    public class RegularExpressionConsts
+    {
+        public const string PhoneNumber = @"^(\+98|0)?9\d{9}$";
+
+        public const string Email = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+    }
+}
